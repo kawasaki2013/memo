7a76e9a8c249ed3ac3ef6671a13ea5f9a841643e
川野忍（shinobu.kawano） <kawanoshinobu@gmail.com>
2012-12-23 16:44:55 +0900

    Add icon etc

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ Ext.application({
     views: ['Main', 'List', 'Form'],
 
     icon: {
-        '57': 'resources/icons/Icon.png',
-        '72': 'resources/icons/Icon~ipad.png',
-        '114': 'resources/icons/Icon@2x.png',
-        '144': 'resources/icons/Icon~ipad@2x.png'
+        '57': 'resources/icons/ios-memo-icon.png',
+        '72': 'resources/icons/ios-memo-icon.png',
+        '114': 'resources/icons/ios-memo-icon.png',
+        '144': 'resources/icons/ios-memo-icon.png'
     },
 
     isIconPrecomposed: true,
